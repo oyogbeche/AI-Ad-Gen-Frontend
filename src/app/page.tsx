d2dee273f5f1c741d6c8f266a0de4cb76abdf65d
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-03-04 14:36:24 +0100

    removed preview icons from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,6 @@ export default function Home() {
       <h1 className="text-4xl py-6 text-center font-bold">
         AI Adgen
       </h1>
-      <Layout />
-      <Headphone />
-      <Lens />
-      <Mail />
     </>
   );
 }
