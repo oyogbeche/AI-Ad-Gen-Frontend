dd6d7d8b451a569cd778e4c72f2159f6ae403b07
victorvalour <ewehvictor7@gmail.com>
2025-03-05 08:43:21 +0100

    feat: add FeaturesSection component with key features display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Headphone, Layout, Lens, Mail } from "@/components/icons/icon";
+import FeaturesSection from "@/components/key-features";
 
 export default function Home() {
   return (
     <>
+    
+    <FeaturesSection />
+
       <h1 className="text-4xl py-6 text-center font-bold">
         AI Adgen
       </h1>
