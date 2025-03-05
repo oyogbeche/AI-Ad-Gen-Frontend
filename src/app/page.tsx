d68264b5879f2bab9251d175edcf90005d368073
Elegbede Oluwaferanmi <oluwaferanmi.joshua@yahoo.com>
2025-03-05 06:36:53 -0800

    new Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@ import FeaturesSection from "@/app/sections/key-features";
 import HeroSection from "@/app/sections/hero";
 import Pricing from "./sections/pricing";
 import Footer from "@/app/sections/footer";
+import FAQ from "@/app/sections/FAQ";
+import CreateAd from "@/app/sections/createAD"; // Import the CreateAd component
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeaturesSection />
+      <CreateAd /> {/* Add the CreateAd component here */}
       <Pricing />
+      <FAQ />
       <Footer />
     </main>
   );
