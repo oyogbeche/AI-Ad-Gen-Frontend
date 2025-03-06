60823534e60c27c1710fd03cf8d2ab3119afd6db
victorvalour <ewehvictor7@gmail.com>
2025-03-06 08:08:21 +0100

    feat: added SignIn, AuthNav and AuthFooter components, implement LoadingButton, and integrate Radix UI Checkbox

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import {SignInForm} from "./sign-in-form";
+//import { useRouter } from "next/navigation";
+//import { useEffect } from "react";
+
+const Login = () => {
+
+
+
+  //const router = useRouter();
+
+/*   useEffect(() => {
+    if (status === "authenticated") {
+      // Redirect to the dashboard if the user is authenticated
+      router.push("/dashboard");
+    }
+  }, [status, router]); */
+
+/*   if (status === "loading") {
+    return <div>Loading...</div>; // Optionally, add a loading state
+  } */
+
+  return (
+   <div className="pt-[30px] px-3 bg-[#F9FAFB]">
+        <SignInForm  />
+        </div>
+  );
+};
+
+export default Login;
