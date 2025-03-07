5fe8dedcfa72c1748cad909636728efca5162dd4
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-03-07 03:45:15 +0100

    style: update font family

--- src/app/(adgen)/layout.tsx
+++ src/app/(adgen)/layout.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
 
 import { TopProgressBarProvider } from "@/lib/nprogress/top-progress-bar-provider";
 import QueryProvider from "@/lib/react-query/query-provider";
@@ -13,12 +12,6 @@ import Header from "@/app/sections/header";
 import { SonnerToaster } from "../sections/sonner-toaster";
 
 
-
-const manrope = Manrope({
-  variable: "--font-manrope",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "AI Adgen",
   description: "AI Adgen",
@@ -30,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={clsx(manrope.variable)}>
+    <html lang="en">
       <body className="font-nunito bg-[#F9FAFB]" cz-shortcut-listen="true">
         <SonnerToaster />
         <QueryProvider>
