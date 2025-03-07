9ee7273ef1058032c5ef7f9e410ef5bb0d6ac345
olaleyeag.20 <olaleyeag.20@student.funaab.edu.ng>
2025-03-07 14:55:38 +0100

    add(favicon): add new favicon

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -4,17 +4,31 @@ import { TopProgressBarProvider } from "@/lib/nprogress/top-progress-bar-provide
 import QueryProvider from "@/lib/react-query/query-provider";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import { Manrope, Nunito } from "next/font/google";
 
 import "./../globals.css";
 
 import Header from "@/app/sections/header";
 import Footer from "../sections/footer";
 import { SonnerToaster } from "../sections/sonner-toaster";
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: "AI Adgen",
   description: "AI Adgen",
 };
+
+// manrope and nunito font from "next/font/google".
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+const nunito = Nunito({
+  variable: '--font-nunito',
+  subsets: ['latin'],
+  weight: ["300", "400", "500", "700", "900"]
+});
 
 export default function RootLayout({
   children,
@@ -22,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(manrope.variable, nunito.variable)}>
       <body className="font-nunito bg-[#F9FAFB] overflow-x-hidden" cz-shortcut-listen="true">
         <SonnerToaster />
         <QueryProvider>
