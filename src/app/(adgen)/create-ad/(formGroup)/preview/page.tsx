d48efdb81522c396d67c4771bd7479ea3d1006c1
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-03-07 04:58:06 +0100

    style: adjust padding and margins and change text styles

--- src/app/(adgen)/create-ad/(formGroup)/preview/page.tsx
+++ src/app/(adgen)/create-ad/(formGroup)/preview/page.tsx
@@ -1,41 +1,48 @@
 import PreviewContent from "./_components/preview-content";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import BackButton from "@/components/back-button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Page() {
   return (
-    <section className="flex flex-col items-center justify-center gap-8 w-full max-w-[879px] mx-auto bg-white rounded-[20px] py-18">
-      <Card className="w-full max-w-[890px]">
-        <CardContent className="p-14">
+    <section className="flex flex-col items-center justify-center gap-8 w-full max-w-[879px] mx-auto rounded-[20px] pt-10 pb-[103px] px-6">
+      <Card className="w-full max-w-[890px] border-none shadow-none py-0">
+        <CardContent className="py-6 px-4 md:px-8">
           <BackButton className="mb-8" />
 
-          <CardHeader className="p-0 mb-6 text-center">
-            <CardTitle className="text-2xl font-bold">
+          <CardHeader className="mb-6 md:mb-8 text-left md:text-center px-0">
+            <CardTitle className="text-[28px] leading-[36px] text-[#121316] font-semibold">
               Let&apos;s set up your Image Ad
             </CardTitle>
-            <p className="text-gray-500 mt-2">
+            <p className="text-[#667185] text-[18px] font-normal mt-1">
               Fill in the details below, then AI generates your ad instantly.
             </p>
           </CardHeader>
 
-          <div className="mb-8">
-            <div className="flex justify-around items-center">
+          <div className="mb-6 md:mb-8">
+            <div className="flex justify-around items-center max-md:mr-4">
               <div className="text-center">
-                <p className="text-sm text-black font-medium">STEP 1</p>
-                <p className="text-xs mt-1 text-gray-700">Set Ad goals</p>
+                <p className="text-xs font-semibold leading-4 text-[#121316]">
+                  STEP 1
+                </p>
+                <p className="text-sm mt-[3px] font-bold leading-5 text-[#121316]">
+                  Set Ad goals
+                </p>
               </div>
-
               <div className="text-center">
-                <p className="text-sm text-gray-400 font-medium">STEP 2</p>
-                <p className="text-xs mt-1 text-gray-400">Preview</p>
+                <p className="text-xs font-semibold leading-4 text-[#121316]">
+                  STEP 2
+                </p>
+                <p className="text-sm mt-[3px] font-bold leading-5 text-[#121316]">
+                  Preview
+                </p>
               </div>
             </div>
 
-            <div className="relative w-full h-2 bg-white-200 rounded-full mt-4 mb-4">
-              <div className="absolute left-0 h-2 bg-[#1467C5] rounded-l-full w-[48%]"></div>
+            <div className="relative w-full h-2.5 bg-white-200 rounded-full mt-6">
+              <div className="absolute left-0 h-2 bg-[#1467C5] rounded-full w-[48%] md:w-[49%]"></div>
 
-              <div className="absolute right-0 h-2 bg-[#1467c5] rounded-r-full w-[48%]"></div>
+              <div className="absolute right-0 h-2 bg-[#1467c5] rounded-full w-[48%] md:w-[49%]"></div>
             </div>
           </div>
           <PreviewContent />
