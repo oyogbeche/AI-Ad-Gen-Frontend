51a6c33e7b5a8c44a4b12972a2a8b3cae3228c36
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-03-07 20:03:32 +0100

    feat: implement ad stepper for ad form and preview

--- src/app/(adgen)/create-ad/(formGroup)/preview/page.tsx
+++ src/app/(adgen)/create-ad/(formGroup)/preview/page.tsx
@@ -1,7 +1,7 @@
-
 import BackButton from "@/components/back-button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Loader from "@/components/ui/loader";
+import Image from "next/image";
 import { Suspense } from "react";
 import SinglePreview from "./_components/single-image-preview";
 
@@ -21,37 +21,32 @@ export default function Page() {
 
             <CardHeader className="mb-6 md:mb-8 text-left md:text-center px-0">
               <CardTitle className="text-[28px] leading-[36px] text-[#121316] font-semibold">
-                Let&apos;s set up your Image Ad
+                All Done!
               </CardTitle>
               <p className="text-[#667185] text-[18px] font-normal mt-1">
-                Fill in the details below, then AI generates your ad instantly.
+                Below is your AI generated Ad Campaign
               </p>
             </CardHeader>
 
             <div className="mb-6 md:mb-8">
-              <div className="flex justify-around items-center max-md:mr-4">
-                <div className="text-center">
-                  <p className="text-xs font-semibold leading-4 text-[#121316]">
-                    STEP 1
-                  </p>
-                  <p className="text-sm mt-[3px] font-bold leading-5 text-[#121316]">
-                    Set Ad goals
-                  </p>
+              <div className="max-w-[342px] w-full mx-auto flex flex-col gap-6 ">
+                <div className="flex items-center justify-center gap-1 :max-w-[295px] w-full mx-auto ">
+                  <Image
+                    className=""
+                    src="/preview-tick.svg"
+                    height={24}
+                    width={24}
+                    alt="Tick icon"
+                    priority
+                  />
+                  <div className="h-1 max-w-[230px] md:max-w-[239px] w-full bg-[#458DE1] rounded-full"></div>
+                  <div className="w-6 h-6 border-3 border-[#458DE1] rounded-full"></div>
                 </div>
-                <div className="text-center">
-                  <p className="text-xs font-semibold leading-4 text-[#121316]">
-                    STEP 2
-                  </p>
-                  <p className="text-sm mt-[3px] font-bold leading-5 text-[#121316]">
-                    Preview
-                  </p>
+
+                <div className="w-full flex items-center justify-between text-base font-bold leading-5 text-[#458DE1]">
+                  <p>Enter Ad Details</p>
+                  <p>Your Generated Ad</p>
                 </div>
-              </div>
-
-              <div className="relative w-full h-2.5 bg-white-200 rounded-full mt-6">
-                <div className="absolute left-0 h-2 bg-[#1467C5] rounded-full w-[47%] md:w-[49%]"></div>
-
-                <div className="absolute right-0 h-2 bg-[#1467c5] rounded-full w-[47%] md:w-[49%]"></div>
               </div>
             </div>
             <SinglePreview />
