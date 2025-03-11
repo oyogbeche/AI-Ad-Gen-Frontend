54310e9cb3277792d28c42e0a0fdf1a4eada903b
Jomagene <semjomagene@gmail.com>
2025-03-11 11:57:09 +0200

    feat(changeLog) : Implement the body of the page

--- src/app/(external)/changelog/page.tsx
+++ src/app/(external)/changelog/page.tsx
@@ -6,6 +6,20 @@ interface Shape {
   picture: [src: string, alt: string, height: number, width: number];
 }
 
+interface Versions {
+  name: string;
+  deadline: string;
+  status: string;
+  title: string;
+  subtitle: string;
+  content: {
+    item1: string[];
+    item2: string[];
+    item3: string[];
+  };
+  picture: string;
+}
+
 const shapes: Shape[] = [
   {
     positionSize:
@@ -26,6 +40,96 @@ const shapes: Shape[] = [
     positionSize:
       "h-[127px] lg:h-[293px] absolute left-[-48px]  lg:left-[-57px] bottom-[-22px] lg:bottom-[-54px]",
     picture: ["/shapes/obj2.svg", "object 2", 293, 297],
+  },
+];
+
+const versions: Versions[] = [
+  {
+    name: "1.O",
+    deadline: "Mar 16, 2024",
+    status: "Released",
+    title: "AI-Powered Ad Generation",
+    subtitle:
+      "You can now generate AI-powered ads effortlessly, streamlining the ad creation process and making it easier than ever to produce high-quality marketing visuals.",
+    content: {
+      item1: ["Instantly generate professional AI-driven ads."],
+      item2: ["Customize ad elements for a tailored look."],
+      item3: ["Save your favourite designs for future use."],
+    },
+    picture: "version0.svg",
+  },
+  {
+    name: "1.1",
+    deadline: "Mar 16, 2024",
+    status: "released",
+    title: "Export and Download",
+    subtitle:
+      "Now, you have full control over your AI-generated ads! This update lets you download and export your ads so you can save and retrieve them anytime, making your experience more seamless.",
+    content: {
+      item1: ["Download ads in PNG or JPEG format."],
+      item2: ["Save your ads for later use"],
+      item3: ["Retrieve saved ads"],
+    },
+    picture: "version1.svg",
+  },
+  {
+    name: "1.2",
+    deadline: "Mar 16, 2024",
+    status: "released",
+    title: "LLimited Trials & Sign-up Prompt",
+    subtitle:
+      "We’ve introduced a fair usage limit to encourage sign-ups. You can generate up to 5 ads per day before needing to sign up or wait for a reset.",
+    content: {
+      item1: ["Generate 5 ads per day for free"],
+      item2: ["Once you hit the limit, choose between:"],
+      item3: [
+        "Signing up via Google authentication for unlimited access.",
+        "Waiting a few hours for your limit to reset.",
+      ],
+    },
+    picture: "version2.svg",
+  },
+  {
+    name: "1.3",
+    deadline: "Mar 16, 2024",
+    status: "released",
+    title: "Google Authentication",
+    subtitle:
+      "Signing up is now effortless with Google Authentication. No emails, no passwords—just quick access to unlimited ad generation.",
+    content: {
+      item1: ["Sign up with Google for unlimited ads"],
+      item2: ["No email/password required"],
+      item3: ["No email verification—instant access"],
+    },
+    picture: "version3.svg",
+  },
+  {
+    name: "1.4",
+    deadline: "Mar 16, 2024",
+    status: "released",
+    title: "Standalone Ad Pages",
+    subtitle:
+      " Every AI-generated ad now gets a unique page, making sharing easy. Share your ads as independent web pages without requiring users to log in.",
+    content: {
+      item1: ["Sign up with Google for unlimited ads"],
+      item2: ["No email/password required"],
+      item3: ["No email verification—instant access"],
+    },
+    picture: "version4.svg",
+  },
+  {
+    name: "1.5",
+    deadline: "Mar 16, 2024",
+    status: "released",
+    title: "Limited Trials & Sign-up Prompt",
+    subtitle:
+      "We introduced usage limits to encourage user sign-ups. Users can generate only 5 ads per day before needing to sign up or wait for a reset.",
+    content: {
+      item1: ["Users can generate up to 5 ads daily."],
+      item2: ["After reaching the limit, users can either:"],
+      item3: ["Sign up via Google authentication for unlimited access."],
+    },
+    picture: "version5.svg",
   },
 ];
 
@@ -59,22 +163,28 @@ const ChangeLogPage = () => {
         </button>
         <div className="max-w-[756px] m-auto flex flex-col gap-6">
           <h1 className="text-[28px] lg:text-[64px] font-medium text-center text-white">
-            The Evolution of Genz.ad
+            What’s New? Changelog & Updates
           </h1>
 
           <p className="text-[12px] md:text-[20px] text-center text-[#ECECEC] font-normal md:font-medium">
-            Here’s a look at what’s new! From exciting features to
-            behind-the-scenes enhancements, we’re continuously improving your
-            experience.
+            HCheck out everything we’ve been working on to make your ad creation
+            smoother, faster, and better!
           </p>
         </div>
       </section>
       <section className="flex flex-col gap-6 md:gap-14 lg:gap-20 max-w-[960px] lg:mt-[103px] pb-[54px] lg:pb-24 mx-[40px] md:mx-[60px] lg:mx-[80px]">
-        <Version />
-        <Version />
-        <Version />
-        <Version />
-        <Version />
+        {versions.map((version) => (
+          <Version
+            key={version.title}
+            name={version.name}
+            deadline={version.deadline}
+            status={version.status}
+            title={version.title}
+            subtitle={version.subtitle}
+            content={version.content}
+            picture={version.picture}
+          />
+        ))}
       </section>
     </main>
   );
--- src/domains/external/components/version.tsx
+++ src/domains/external/components/version.tsx
@@ -1,48 +1,85 @@
 import Image from "next/image";
 
-const Version = () => {
+type MyComponentProps = {
+  key: string;
+  name: string;
+  deadline: string;
+  status: string;
+  title: string;
+  subtitle: string;
+  content: {
+    item1: string[];
+    item2: string[];
+    item3: string[];
+  };
+  picture: string;
+};
+
+const Version: React.FC<MyComponentProps> = ({
+  name,
+  deadline,
+  status,
+  title,
+  subtitle,
+  content,
+  picture,
+}) => {
   return (
     <div className="flex flex-col md:flex-row w-full gap-10 md:gap-20">
       <div className="w-[123px]">
         <h2 className="text-[18px] md:text-[24px] font-semibold pb-3 md:pb-6">
-          Version 1.0
+          {`Version ${name}`}
         </h2>
         <p className="text-[14px] md:text-[16px] text-[#121316] pb-2.5 md:pb-4">
-          Mar 16, 2024
+          {deadline}
         </p>
         <button className="text-[12px] flex items-center gap-1.5 px-2 py-1 bg-[#E7F5EC] rounded-[30px] text-[#0F973D] font-semibold">
-          <span>Released</span>
+          <span>{status}</span>
           <Image src="/done.svg" alt="done" height={12} width={12} />
         </button>
       </div>
       <div>
         <hgroup>
           <h1 className="text-2xl font-medium md:font-semibold md:text-[32px] pb-2 ">
-            Generate Image Ads
+            {title}
           </h1>
           <p className="text-[14px] md:text-[16px] text-[#121316]">
-            Users can enter and generate image Ads based on{" "}
+            {subtitle}
           </p>
         </hgroup>
         <ul className="text-[#5F5F5F] pt-6 list-disc pl-4">
-          <li> Users can input key campaign details, including:</li>
-          <div className="pl-5">
-            <li> Ad goal (description of the ad's goal).</li>
-            <li>
-              Target audience attributes (e.g., region, language, demographics).
-            </li>
-            <li>
-              Ad size selection (Instagram Post, Facebook Ad, LinkedIn Banner,
-              Google Display Ads, etc.).
-            </li>
-          </div>
-          <li>
-            AI automatically generates an image ad campaign structure based on
-            the user inputs.
-          </li>
+          {content?.item1.length > 1 ? (
+            content?.item1.map((itemContent) => (
+              <div className="pl-5">
+                <li>{itemContent}</li>
+              </div>
+            ))
+          ) : (
+            <li>{content?.item1[0]}</li>
+          )}
+
+          {content?.item2.length > 1 ? (
+            content?.item2.map((itemContent) => (
+              <div className="pl-5">
+                <li>{itemContent}</li>
+              </div>
+            ))
+          ) : (
+            <li>{content?.item2[0]}</li>
+          )}
+
+          {content?.item3.length > 1 ? (
+            content?.item3.map((itemContent) => (
+              <div className="pl-5">
+                <li>{itemContent}</li>
+              </div>
+            ))
+          ) : (
+            <li>{content?.item2[0]}</li>
+          )}
         </ul>
         <Image
-          src="/img1.svg"
+          src={picture}
           alt="version 1.1"
           width={757}
           height={480}
