4accbd483930aab1724f26b1734a72ef06485562
emaximus <emmschuks18@gmail.com>
2025-03-20 10:40:42 +0100

    comment out import react statement

--- src/app/(other-externals)/pricings/page.tsx
+++ src/app/(other-externals)/pricings/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import react from "react";
+// import react from "react";
 import Link from "next/link";
 import ToggleSwitch from "../components/toggle-switch";
 const Pricings = () => {
