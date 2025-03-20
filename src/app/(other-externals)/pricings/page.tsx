b838dff6dab5ab639f2289fa13cda22b9be397fb
emaximus <emmschuks18@gmail.com>
2025-03-20 12:01:25 +0100

    refactor pricing page UI

--- src/app/(other-externals)/pricings/page.tsx
+++ src/app/(other-externals)/pricings/page.tsx
@@ -11,7 +11,7 @@ const Pricings = () => {
         </h1>
         <p className="md:font-[500] md:text-[18px] text-[#7D7D7D] font-[500] text-[12px]">Generate your ads for free and pay as you grow.</p>
       </div>
-      <div className="pt-[30px] pb-[50px] hidden lg:inline">
+      {/* <div className="pt-[30px] pb-[50px] hidden lg:inline">
         <p className="flex flex-row items-center justify-center font-[500] text-[16px] text-[#5F5F5F]">
           <span className="pr-[13px]">Bill Monthly</span>
           <svg width="36" height="20" viewBox="0 0 36 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -20,13 +20,13 @@ const Pricings = () => {
           </svg>
           <span className="pl-[13px]">Bill Annually (10% off)</span>
         </p>
-      </div>
+      </div> */}
       {/* toggler component */}
-      <div>
-      <div className="flex justify-center items-center lg:hidden pt-[15px] pb-[25px]">
-      <ToggleSwitch />
-    </div> 
-      </div>
+      {/* <div>
+        <div className="flex justify-center items-center lg:hidden pt-[15px] pb-[25px]">
+          <ToggleSwitch />
+        </div>
+      </div> */}
 
       {/* Card Components */}
       <div className="flex lg:flex-row flex-col gap-[16px] lg:gap-[24px] pb-[30px]">
@@ -35,27 +35,25 @@ const Pricings = () => {
           <div className="px-[36px] py-[39px] flex flex-col">
             <div>
               <div className="h-[76px]">
-                <h3 className="font-[600] text-[28px] text-[#121316]">Starter</h3>
-                <p className="font-[400] text-[14px] text-[#7D7D7D] w-[230px]">Get a few features to start your Genz.ad-ai journey.</p>
+                <h3 className="font-[600] text-[28px] text-[#121316]">Pay-Per-Ad Model</h3>
+                <p className="font-[400] text-[14px] text-[#7D7D7D] w-[230px]">(For non-subscribers)</p>
               </div>
               <div>
-                <h1 className="font-[600] text-[36px] text-[#B800B8] mt-[17px] border-b-[1px] border-b-[#E3E3E3]">Free</h1>
+                <h1 className="font-[600] text-[36px] text-[#B800B8] mt-[17px] border-b-[1px] border-b-[#E3E3E3]">$50<sub className="font-[400] text-[14px]">/150 Points   </sub></h1>
               </div>
             </div>
-            <div className="mt-[44px]">
-              <span className="pr-[14px]">&#x2713;</span> 5 ads per month <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Watermarked exports <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Limited selection of templates <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Standard resolution only (720p) <br />
-            </div>
 
-            <div className="pt-[53px]">
+            <div className="pt-[16px]">
               <Link
                 href="#"
-                className="bg-light-purple cursor-pointer text-white px-6 py-3 rounded-sm hover:bg-dark-purple transition-colors flex items-center justify-center gap-2 w-fit mx-auto"
+                className="bg-light-purple cursor-pointer text-white px-6 py-[12px] rounded-sm hover:bg-dark-purple transition-colors flex items-center justify-center gap-2 mx-auto"
               >
                 <p>Choose plan</p>
               </Link>
+            </div>
+
+            <div className="mt-[24px]">
+              <span className="pr-[14px]">&#x2713;</span> 150 Ad Credits <br />
             </div>
 
           </div>
@@ -67,26 +65,30 @@ const Pricings = () => {
             <div>
               <div className="h-[76px]">
                 <h3 className="font-[600] text-[28px] text-[#FFFFFF]">Basic</h3>
-                <p className="font-[400] text-[14px] text-[#FFFFFF] w-[230px]">Get full usage of the generator</p>
+                <p className="font-[400] text-[14px] text-[#FFFFFF]">Get a few features to start your Genz.ad journey.</p>
               </div>
               <div>
-                <h1 className="font-[600] text-[36px] text-[#FFFFFF] mt-[17px] border-b-[1px] border-b-[#E3E3E3]">$30<sub className="font-[400] text-[14px]">/mo   </sub></h1>
+                <h1 className="font-[600] text-[36px] text-[#FFFFFF] mt-[17px] border-b-[1px] border-b-[#E3E3E3]">$10<sub className="font-[400] text-[14px]">/mo   </sub></h1>
               </div>
             </div>
-            <div className="mt-[44px] text-[#FFFFFF]">
-              <span className="pr-[14px]">&#x2713;</span> Unlimited generated ads <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> No watermarked exports <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Access to premuim templates <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Exports in HD quality (1080p) <br />
-            </div>
 
-            <div className="pt-[53px]">
+            <div className="pt-[16px]">
               <Link
                 href="#"
-                className="bg-[#FFFFFF] cursor-pointer text-[#520052] px-6 py-3 rounded-sm transition-colors flex items-center justify-center gap-2 w-fit mx-auto"
+                className="bg-[#FFFFFF] cursor-pointer text-[#520052] px-6 py-[12px] rounded-sm transition-colors flex items-center justify-center gap-2 mx-auto"
               >
                 <p>Choose plan</p>
               </Link>
+            </div>
+
+            <div className="mt-[44px] text-[#FFFFFF]">
+              <span className="pr-[14px]">&#x2713;</span> Access to predefined prompts <br />
+              <br /><span className="pr-[14px]">&#x2713;</span> 50 ad credit <br />
+              <br /><span className="pr-[14px]">&#x2713;</span> Advanced AI-powered image and text editing  <br />
+              <br /><span className="pr-[14px]">&#x2713;</span> Download the image ad generated <br />
+              <br /><span className="pr-[14px]">&#x2713;</span> Access to explore our community <br />
+              <br /><span className="pr-[14px]">&#x2713;</span> Ad text and image editing tools <br />
+              <br /><span className="pr-[14px]">&#x2713;</span> Expires after 1 month <br />
             </div>
 
           </div>
@@ -97,32 +99,38 @@ const Pricings = () => {
           <div className="px-[36px] py-[39px] flex flex-col">
             <div>
               <div className="h-[76px]">
-                <h3 className="font-[600] text-[28px] text-[#121316]">Teams</h3>
-                <p className="font-[400] text-[14px] text-[#7D7D7D] w-[230px]">A full package for a 3-4 membered team.</p>
+                <h3 className="font-[600] text-[28px] text-[#121316]">Pro</h3>
+                <p className="font-[400] text-[14px] text-[#7D7D7D] w-[230px]">Get full usage of the generator.</p>
               </div>
               <div>
-                <h1 className="font-[600] text-[36px] text-[#B800B8] mt-[17px] border-b-[1px] border-b-[#E3E3E3]">$50<sub className="font-[400] text-[14px]">/mo   </sub></h1>
+                <h1 className="font-[600] text-[36px] text-[#B800B8] mt-[17px] border-b-[1px] border-b-[#E3E3E3]">$21<sub className="font-[400] text-[14px]">/mo</sub></h1>
               </div>
             </div>
-            <div className="mt-[44px]">
-              <span className="pr-[14px]">&#x2713;</span> Unlimited generated ads <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Share with team members <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> Team brand kit integration <br />
-              <br /><span className="pr-[14px]">&#x2713;</span> 4K resolution & custom export options <br />
-            </div>
 
-            <div className="pt-[53px]">
+            <div className="pt-[16px]">
               <Link
                 href="#"
-                className="bg-light-purple cursor-pointer text-white px-6 py-3 rounded-sm hover:bg-dark-purple transition-colors flex items-center justify-center gap-2 w-fit mx-auto"
+                className="bg-light-purple cursor-pointer text-white px-6 py-[12px] rounded-sm hover:bg-dark-purple transition-colors flex items-center justify-center gap-2 mx-auto"
               >
                 <p>Choose plan</p>
               </Link>
             </div>
 
+            <div className="mt-[44px] flex flex-col gap-[16px]">
+              <p><span className="pr-[14px]">&#x2713;</span> All Basic features</p>
+              <p><span className="pr-[14px]">&#x2713;</span> 150 ad credits</p>
+              <p><span className="pr-[14px]">&#x2713;</span> Advanced AI-powered image and text editing (Inpainting)</p>
+              <p><span className="pr-[14px]">&#x2713;</span> High-resolution downloads (PNG/JPEG)</p>
+              <p><span className="pr-[14px]">&#x2713;</span> Access to explore our community</p>
+              <p><span className="pr-[14px]">&#x2713;</span> Expires after 3 months</p>
+              <p><span className="pr-[14px]">&#x2713;</span> Rollover ad credits on resubscription.</p>
+            </div>
+
           </div>
         </div>
       </div>
+
+      <p className="pb-[73px]">Just getting started? <a href="#" className="text-[#B800B8] underline">Try our free plan</a></p>
     </div>
   );
 };
