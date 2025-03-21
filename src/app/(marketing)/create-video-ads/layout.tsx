b7b6c6b209b8c5d0536036d7089fc0543ad43b44
Chris <victorchris73@gmail.com>
2025-03-21 23:41:04 +0100

    components: updated footer and header components

--- src/app/(marketing)/create-video-ads/layout.tsx
+++ src/app/(marketing)/create-video-ads/layout.tsx
@@ -1,12 +1,15 @@
 import Header from "@/domains/external/components/header";
-import Footer from "@/domains/external/components/footer";
+// import Footer from "@/domains/external/components/footer";
+import BlogFooter from "@/app/(other-externals)/components/blog-footer";
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) { 
   return ( 
     <div> 
       <Header /> 
       <main>{children}</main> 
-      <Footer /> 
+      <div className="max-w-7xl w-full xl:mx-auto max-lg:px-4 pt-8">
+              <BlogFooter />
+            </div>
     </div> 
   ); 
 }
