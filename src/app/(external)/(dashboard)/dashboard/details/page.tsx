f72eec63fcb7a7afae46f7b7778bf0d3d23f6bf0
olaoluwajo <olagundoyealexander@gmail.com>
2025-03-21 10:30:30 +0100

    Refactor save button logic and add video link to features section

--- src/app/(external)/(dashboard)/dashboard/details/page.tsx
+++ src/app/(external)/(dashboard)/dashboard/details/page.tsx
@@ -129,7 +129,7 @@ const AddDetails = () => {
             handleCopy={handleCopy}
             status="completed"
             type="image-form"
-            hideSaveButton={true}
+            hideSaveAndExit={true}
           />
         </div>
         <picture className="md:my-10 py-3 bg-[#F0F3F5] rounded-lg md:p-10  max-w-[699px] w-full max-md:w-[90%] flex items-center justify-center">
