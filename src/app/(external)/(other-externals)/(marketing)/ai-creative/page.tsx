ed1076c0d43596aa759a826565b33b3930f63f43
Jomagene <semjomagene@gmail.com>
2025-03-22 14:23:40 +0200

    fix: marge conflict

--- src/app/(external)/(other-externals)/(marketing)/ai-creative/page.tsx
+++ src/app/(external)/(other-externals)/(marketing)/ai-creative/page.tsx
@@ -8,21 +8,14 @@ import Cta from "@/domains/external/components/cta";
 import VideoPlayer from "@/domains/external/components/video-player";
 
 export default function MarketingPage() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <HeroMarketing />
-      <FeaturesSection />
-=======
   const videoLink = "https://www.youtube.com/embed/_qKV8awfIfQ";
-  https: return (
+  return (
     <div>
       <HeroMarketing />
       <FeaturesSection />
       <div className="px-4">
         <VideoPlayer videoLink={videoLink} />
       </div>
->>>>>>> 46592e4dfe5d427d1dcff9b7c92f5a519c1a6993
       <CreateAds />
       <CreateAd />
       <FAQ />
