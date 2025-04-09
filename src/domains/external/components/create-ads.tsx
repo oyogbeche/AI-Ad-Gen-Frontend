5a99f3801f201dcc952c7aef6f012a5bcb69716f
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-04-10 00:07:50 +0100

    chore: remove unused images and move used images in components dir

--- src/domains/external/components/create-ads.tsx
+++ src/domains/external/components/create-ads.tsx
@@ -1,10 +1,9 @@
 "use client";
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
-import createadImg from "@/components/images/createadsImg.svg";
-import circle from "@/components/icons/circle.svg";
-import { motion } from "framer-motion";
-import { useInView } from "framer-motion";
 import { useRef } from "react";
+import circle from "../../../../public/circle.svg";
+import createadImg from "../../../../public/createadsImg.svg";
 
 const notes = [
   {
