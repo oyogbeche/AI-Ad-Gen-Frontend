6d7f54c3618d4de66aed43b3af5a55e414121771
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-04-10 15:32:03 +0100

    chore: remove unused components and API-related files in the domains dir

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { ProtectedRoute } from "@/app/(auth)/components/protected-route";
 import { AdsProvider } from "@/domains/ads-gen/context/AdsContext";
+import Header from "@/domains/external/components/header";
 
 export default function RootLayout({
   children,
@@ -9,6 +10,7 @@ export default function RootLayout({
   return (
     <>
       <ProtectedRoute>
+        <Header />
         <AdsProvider>{children}</AdsProvider>
       </ProtectedRoute>
     </>
